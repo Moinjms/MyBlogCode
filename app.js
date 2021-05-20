07516bc36b125be72cb01151f41b1005c62d4aa4
Moinjms <83553981+Moinjms@users.noreply.github.com>
2021-05-19 22:26:11 -0500

    My Blog EJS Completed Version 3 Final

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "HOME Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "ABOUT Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "CONTACT Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const today = new Date();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -18,8 +21,11 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
+  // console.log(posts);
   res.render("home", {
-    para1: homeStartingContent
+    para1: homeStartingContent,
+    posts: posts,
+    todaysDate: today
   });
 });
 
@@ -40,9 +46,30 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  console.log(req.body.entry );
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postText
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
+// Calling using params
+//************************************************
+// app.get("/a/:x", function(req, res) {
+//************************************************
+app.get("/posts/:postPage", function(req, res) {
+  const newPage = _.lowerCase(req.params.postPage);
+  posts.forEach(function(post) {
+    if (newPage === _.lowerCase(post.title)) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    };
+  });
+});
+// ***********************************************
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
